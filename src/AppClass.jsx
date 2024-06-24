096ed87374f53bda44992802e5cc8f0a7eea7bd0
mahimahamsa <pji.mahima.hamsa@kalvium.community>
2024-06-24 11:47:20 +0530

    new

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -27,4 +27,20 @@ export default class AppClass extends Component{
   }
 
   // code here
+  render() {
+    
+    const images1 = this.imageData();
+    return (
+      <div className='App'>
+        <h1>Kalvium Gallery</h1>
+        <div className="image-grid">
+          <img key={images1[0].id} src={images1[0].img} alt="Image Description" />
+          <img key={images1[1].id} src={images1[1].img} alt="Image Description" />
+          <img key={images1[2].id} src={images1[2].img} alt="Image Description" />
+          <img key={images1[3].id} src={images1[3].img} alt="Image Description" />
+        </div>
+      </div>
+    )
+  }
+  
 }
